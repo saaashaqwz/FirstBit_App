6ba3ca4c735bdc0239aebfb57b09848211604f6f
Sasha <228884RUS@mail.ru>
2025-11-10 15:18:31 +0500

    feat: добавлена UI главной страницы с навигацией

---
+++ app/src/main/java/com/example/firstbit_app/UI/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.firstbit_app.UI;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.firstbit_app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+    }
+}
